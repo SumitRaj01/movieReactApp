78fef9eac3121572e5703edc079afdbca302a941
SumitRaj01 <Sumeet23051994@gmail.com>
2022-06-10 12:47:56 +0530

    movie list added from api
    movie list added

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,59 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 function App() {
-  return <div>Hello World</div>;
+  //add a state object which is going to hold the movie result that come back from the search
+  const [movies, setMovies] = useState([
+    {
+      Title: "Star Wars",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode V - The Empire Strikes Back",
+      Year: "1980",
+      imdbID: "tt0080684",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VII - The Force Awakens",
+      Year: "2015",
+      imdbID: "tt2488496",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode I - The Phantom Menace",
+      Year: "1999",
+      imdbID: "tt0120915",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+  ]);
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
