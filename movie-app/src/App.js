20abcaac48c4e14980f695320c0feedbedc3327a
SumitRaj01 <Sumeet23051994@gmail.com>
2022-06-12 12:23:07 +0530

    fixed bug
    https

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -14,13 +14,10 @@ function App() {
 
   //Adding a call to the API so that you can get the films from the API
   const getMovieRequest = async (searchValue) => {
-    // const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=1450179`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=1450179`;
 
     //using fetch API to make a request
-    // const response = await fetch(url);
-    const response = await fetch(
-      `https://www.omdbapi.com/?s=${searchValue}&apikey=1450179`
-    );
+    const response = await fetch(url);
 
     //converting http response to json
     const responseJson = await response.json();
